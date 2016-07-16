a480c445b91ea3c8aaa89ce7763e6026e1c49481
Ramiro Pinol <ramirognr@gmail.com>
2016-07-16 02:12:31 +0000

    first working version

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 var express = require("express")
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello Cruel World!');
+var tmstp = function (input) {
+    if (isNaN(Number(input))) {
+        var check = (new Date(input)).getTime();
+    } else {
+        var check = (new Date(Number(input))).getTime();
+    }
+    
+    if (check > 0) {
+      var tostr = new Date(check).toDateString()
+      return JSON.stringify({unix : check, natural : tostr})
+    } else {
+        return JSON.stringify({unix : null, natural : null});
+    }
+}
+
+app.get('/:ID', function (req, res) {
+    res.send(tmstp(req.params.ID));
+    
 });
 
 app.listen(8080, function () {
